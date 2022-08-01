1d710fbd36ef53d3b712bf863f2ad76fd70d1be3
BossiaK-Gaming <76888518+BossiaK@users.noreply.github.com>
2022-08-01 23:13:20 +0200

    Update android_build_env.sh

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -39,7 +39,7 @@ sudo DEBIAN_FRONTEND=noninteractive \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
     libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
-    maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+    maven rsync ncftp ncurses-dev patch patchelf pkg-config pngcrush \
     pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
     libxml-simple-perl libswitch-perl apt-utils \
